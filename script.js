05f7312dbb07967a853d93ce6b8eac5e7767c41d
Sulemank-1 <suleman.dev42@gmail.com>
2025-06-07 01:54:08 +0500

    Some changes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let num1 = 0;
 let num2 = 0;
 let operator = "";
+const displayArea = document.querySelector(".display");
+let displayContent = 0;
 
 function add(a, b) {
   return a + b;
@@ -24,4 +26,13 @@ function operate(num1, operator, num2) {
   else if (operator === "*") return multiply(num1, num2);
   else if (operator === "/") return divide(num1, num2);
   else window.alert("Wrong operator");
+}
+
+function display(digit) {
+  displayArea.textContent += digit;
+  if (digit >= 0 && digit <= 9) {
+    num1 = digit;
+  } else if (["+", "-", "x", "/", ".", "="].includes(digit)) {
+    operator = digit;
+  }
 }
